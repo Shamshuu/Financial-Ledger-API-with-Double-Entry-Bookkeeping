046c9535f3581f6fd12c42969c87c54fddcf3455
Shamshuu <shamshu2004@gmail.com>
2026-05-25 18:52:57 +0530

    Update DataInitializer

--- src/main/java/com/ledger/config/DataInitializer.java
+++ src/main/java/com/ledger/config/DataInitializer.java
@@ -49,8 +49,6 @@ public class DataInitializer implements ApplicationRunner {
         }
     }
 
-    /**
-     */
     private void createImmutabilityTrigger() {
         try {
             jdbcTemplate.execute("""
