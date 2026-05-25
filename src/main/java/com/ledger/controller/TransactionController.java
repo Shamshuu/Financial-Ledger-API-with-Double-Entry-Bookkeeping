450188e71c295af5d541430c952d9fa79c16ec33
Shamshuu <shamshu2004@gmail.com>
2026-05-25 18:57:03 +0530

    Update TransactionController

--- src/main/java/com/ledger/controller/TransactionController.java
+++ src/main/java/com/ledger/controller/TransactionController.java
@@ -39,7 +39,6 @@ public class TransactionController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-
     @PostMapping("/withdrawals")
     public ResponseEntity<TransactionResponse> withdraw(
             @Valid @RequestBody WithdrawalRequest request) {
